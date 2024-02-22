eaa57fc16450fa0d666f43c047db511f206321cc
kailashgautham <kailash.gautham@gmail.com>
2024-02-22 22:58:30 +0800

    Make stylecheck changes

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,45 +1,44 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
-public class Remark {
-
-/*
- * The first character of the remark must not be a whitespace,
- * otherwise " " (a blank string) becomes a valid input.
- */
-public final String value;
 
 /**
- * Constructs an {@code Remark}.
- *
- * @param remark A valid remark.
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable.
  */
-public Remark(String remark) {
-    requireNonNull(remark);
-    value = remark;
-}
+public class Remark {
 
-@Override
-public String toString() {
-    return value;
-}
+    public final String value;
 
-@Override
-public boolean equals(Object other) {
-    if (other == this) {
-        return true;
+    /**
+     * Constructs an {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
     }
 
-    // instanceof handles nulls
-    if (!(other instanceof Remark)) {
-        return false;
+    @Override
+    public String toString() {
+        return value;
     }
 
-    Remark otherRemark = (Remark) other;
-    return value.equals(otherRemark.value);
-}
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return value.equals(otherRemark.value);
+    }
 
     @Override
     public int hashCode() {
