9ae4d770d663ae949deb8fe5bf5594526168b3b7
kailashgautham <kailash.gautham@gmail.com>
2024-02-23 13:48:38 +0800

    Modify test files

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -21,7 +21,7 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_REMARK = "Friend from the animal shelter";
+    public static final String DEFAULT_REMARK = "College friend";
 
     private Name name;
     private Phone phone;
