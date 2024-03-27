e52d02363355b0d326f7563866b43bc8258490ba
HENRY\YS <yusoonng3@gmail.com>
2024-03-27 13:43:19 +0800

    Update Note class

--- src/main/java/seedu/address/model/person/Note.java
+++ src/main/java/seedu/address/model/person/Note.java
@@ -1,109 +1,62 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-
-import java.util.ArrayList;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a note associated with a person in the address book.
+ * Represents a Person's note in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidNote(String)}
  */
 public class Note {
+
     public static final String MESSAGE_CONSTRAINTS =
-            "Note should only contain alphanumeric characters and symbols, and it should not be blank";
+            "Notes should only contain alphanumeric characters and punctuation(.,!?),"
+                    + " and it should not be blank";
+
+    public static final String VALIDATION_REGEX = "^(?=.*[a-zA-Z0-9.,!?])[a-zA-Z0-9.,!? ]+$";
+
+    private final String note;
 
     /**
-     * Represents an empty note.
-     */
-    public static final Note EMPTY = new Note(new ArrayList<>(), true);
-
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
-    public static final String VALIDATION_REGEX = "^[a-zA-Z0-9_.-]+$";
-
-    public final ArrayList<String> notes;
-
-    /**
-     * Constructs a {@code Note} with the specified list of notes.
+     * Constructs a {@code Note}.
      *
-     * @param notes The list of notes.
+     * @param note A valid note.
      */
-    public Note(ArrayList<String> notes) {
-        requireNonNull(notes);
-        this.notes = new ArrayList<>(notes);
+    public Note(String note) {
+        requireNonNull(note);
+        checkArgument(isValidNote(note), MESSAGE_CONSTRAINTS);
+        this.note = note;
     }
 
     /**
-     * Constructs an empty {@code Note}.
-     *
-     * @param isSentinel Boolean flag to differentiate this constructor from the primary constructor.
-     */
-    private Note(ArrayList<String> arrayList, boolean isSentinel) {
-        if (!isSentinel) {
-            throw new IllegalArgumentException("This constructor is only for creating the EMPTY object");
-        }
-        this.notes = arrayList;
-    }
-
-    public void addNote(String noteToAdd) {
-        this.notes.add(noteToAdd);
-    }
-
-    /**
-     * Retrieves the list of notes associated with this Person.
-     *
-     * @return The list of notes associated with this Person.
-     */
-    public ArrayList<String> getNote() {
-        return this.notes;
-    }
-
-    /**
-     * Returns true if a given string is a valid note.
-     *
-     * @param test The string to test.
-     * @return True if the string is a valid note, false otherwise.
+     * Returns true if a given string is a note.
      */
     public static boolean isValidNote(String test) {
         return test.matches(VALIDATION_REGEX);
     }
 
-    /**
-     * Checks if this note is empty.
-     *
-     * @return True if the note is empty, false otherwise.
-     */
-    public boolean isEmpty() {
-        return notes.isEmpty();
-    }
-
-    /**
-     * Returns the string representation of the note.
-     *
-     * @return The string representation of the note.
-     */
     @Override
     public String toString() {
-        if (notes.isEmpty()) {
-            return "";
-        }
-        StringBuilder sb = new StringBuilder();
-        for (String s : notes) {
-            sb.append(s);
-            sb.append("/n");
-        }
-        return sb.toString();
+        return note;
     }
 
-    /**
-     * Returns the hash code of the note.
-     *
-     * @return The hash code of the note.
-     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Note)) {
+            return false;
+        }
+
+        Note otherNote = (Note) other;
+        return note.equals(otherNote.note);
+    }
+
     @Override
     public int hashCode() {
-        return notes.hashCode();
+        return note.hashCode();
     }
 }
