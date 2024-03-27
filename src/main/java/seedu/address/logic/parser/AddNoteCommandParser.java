5982cbd25cc39b5e8da413ce4f32dcf2b8c24351
HENRY\YS <yusoonng3@gmail.com>
2024-03-27 13:46:36 +0800

    Update addnotecommandparser class to use Note

--- src/main/java/seedu/address/logic/parser/AddNoteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddNoteCommandParser.java
@@ -1,42 +1,46 @@
 package seedu.address.logic.parser;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+
+import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AddNoteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Note;
 
 /**
  * Parses input arguments and creates a new AddNoteCommand object.
  */
 public class AddNoteCommandParser implements Parser<AddNoteCommand> {
-    public static final String MESSAGE_ONE_BASED_INDEXING =
-            "Please input a 1-based index.";
-
     /**
-     * Parses the given {@code args} and creates a new AddNoteCommand object.
-     *
-     * @param args The arguments provided for the command in the format: {@code INDEX NOTE}.
-     * @return A new AddNoteCommand object.
-     * @throws ParseException If the input arguments are invalid.
+     * Parses the given {@code String} of arguments in the context of the AddNoteCommand
+     * and returns an AddNoteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
      */
     public AddNoteCommand parse(String args) throws ParseException {
-        requireNonNull(args);
-        args = args.trim();
-        String[] strArr = args.split(" ");
-
-        Index index;
-
-        try {
-            index = ParserUtil.parseIndex(strArr[0]);
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddNoteCommand.MESSAGE_USAGE), pe);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_INDEX, PREFIX_NOTE);
+        if (!arePrefixesPresent(argMultimap, PREFIX_INDEX, PREFIX_NOTE)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddNoteCommand.MESSAGE_USAGE));
         }
-        if (index.getOneBased() <= 0) {
-            throw new ParseException(MESSAGE_ONE_BASED_INDEXING);
-        }
-        return new AddNoteCommand(index, strArr[1]);
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_INDEX, PREFIX_NOTE);
+
+        Index index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_INDEX).get());
+        Note note = ParserUtil.parseNote(argMultimap.getValue(PREFIX_NOTE).get());
+
+        return new AddNoteCommand(index, note);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
